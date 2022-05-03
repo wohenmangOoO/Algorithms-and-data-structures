85e9695f5f29a4af35a3cee64932170fd5c057d4
liujin <liujin103827@163.com>
2022-05-03 13:56:57 +0800

    修改了两个有序链表合并

--- src/com/liujin/code/Code14_MergerTwoSortedLinkedList.java
+++ src/com/liujin/code/Code14_MergerTwoSortedLinkedList.java
@@ -5,6 +5,7 @@ public class Code14_MergerTwoSortedLinkedList {
     public static class ListNode{
         public int val;
         public ListNode next;
+
     }
     public static ListNode mergerTwoSortedLinkedList(ListNode head1, ListNode head2){
         if (head1 == null || head2 == null){

36e82b6fa3c80c8b1750a8bc496435f4252e5ba7
liujin <liujin103827@163.com>
2022-05-12 23:46:58 +0800

    判断一棵树是否是镜面树

---
+++ src/com/liujin/code/Code20_SymmetricTree.java
@@ -0,0 +1,22 @@
+package com.liujin.code;
+// 测试链接：https://leetcode.com/problems/symmetric-tree
+//判断一棵树是否是镜面树
+public class Code20_SymmetricTree {
+    public static class TreeNode{
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+    }
+    public static boolean isMirror(TreeNode h1, TreeNode h2){
+        if (h1 == null ^ h2 == null){
+            return false;
+        }
+        if (h1 == null && h2 == null){
+            return true;
+        }
+        return h1.val == h2.val && isMirror(h1.left, h2.right) && isMirror(h1.right, h2.left);
+    }
+    public static boolean isSymmetricTree(TreeNode root){
+        return isMirror(root, root);
+    }
+}
